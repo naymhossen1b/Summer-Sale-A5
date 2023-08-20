79de36734055465455628bcb7036239067db08a0
Naym Hossen <naym@Nayms-MBP.Dlink>
2023-08-20 16:06:12 +0600

    added and modified some section and js function

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-
-// let total = 0;
+let total = 0;
 
 function clickItem(target) {
-    const selectedItemContainer = document.getElementById("select-item");
-    const itemName = target.parentNode.childNodes[3].innerText;
-    const li = document.createElement("li");
-    li.innerText = itemName;
-    selectedItemContainer.appendChild(li);
-    
-    console.log(target.parentNode.childNodes[5].innerText.split(" ")[5]);
-    // const price = ;
 
-    // total = parseInt(total) + parseInt(price);
-    // document.getElementById("total").innerText = total;
+  const selectedItemContainer = document.getElementById("select-item");
+  const itemName = target.childNodes[5].childNodes[1].innerText;
+  const li = document.createElement("li");
+  li.innerText = itemName;
+  selectedItemContainer.appendChild(li);
+  const proDuctPrice = target.childNodes[5].childNodes[3].innerText.split(" ")[0];
+  total = parseInt(total) + parseInt(proDuctPrice);
+  document.getElementById("orginal-price").innerText = total;
+
+}
+
+function applyCoupon(){
+  const couponCode = document.getElementById("couponCode").value;
+  if(couponCode === "SELL200" && total > 200 ){
+    const discountParcentage = 20;
+    const discount = (total * discountParcentage) / 100;
+    const finalAmount = total - discount;
+    document.getElementById("discountAmount").textContent = discount;
+    document.getElementById("finalAmount").textContent = finalAmount;
+  }else{
+    alert("Coupon code is not valid or minimum amount not valide");
   }
+}
+
+
+document.getElementById('couponCode').addEventListener('keyup', function(event){
+
+  const text = event.target.value;
+  const sellCuponDelite = document.getElementById('')
+})
+
+
